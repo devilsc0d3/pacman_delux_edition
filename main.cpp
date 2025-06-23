f2f280dcfda8c1984fba44812e8073f373b22958
devilsc0d3 <faureleo736@gmail.com>
2025-06-23 14:49:42 +0200

    feat: add menu

--- main.cpp
+++ main.cpp
@@ -1,11 +1,41 @@
-#include "mainwindow.h"
+// #include "mainwindow.h"
 
+// #include <QApplication>
+
+// int main(int argc, char *argv[])
+// {
+//     QApplication a(argc, argv);
+//     MainWindow w;
+//     w.show();
+//     return a.exec();
+// }
 #include <QApplication>
+#include <QMainWindow>
+#include <QMediaPlayer>
+#include <QMediaPlaylist>
+#include <QUrl>
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    QMainWindow window;
+    window.setWindowTitle("PacMan Neon");
+
+    // 🎵 Création d’un lecteur de musique
+    QMediaPlayer *player = new QMediaPlayer(&window);
+
+    // (Facultatif) Lire en boucle :
+    QMediaPlaylist *playlist = new QMediaPlaylist();
+    playlist->addMedia(QUrl::fromLocalFile("pacman_theme.mp3"));
+    playlist->setPlaybackMode(QMediaPlaylist::Loop);
+    player->setPlaylist(playlist);
+
+    player->setVolume(50);
+    player->play();
+
+    window.resize(800, 600);
+    window.show();
+
+    return app.exec();
 }
+

1fd061d99bbcfe058a87398f0bdd9d2a7919bf86
MedericFedora <kyomamh@gmail.com>
2025-06-24 14:58:40 +0200

    Merge

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <QMainWindow> // Ou juste QWidget
 #include "map.h"
 #include "MapView.h"
+#include "mainwindow.h"
 // Incluez d'autres fichiers si nécessaire, comme PacGumFactory
 
 int main(int argc, char *argv[])
@@ -13,8 +14,8 @@ int main(int argc, char *argv[])
     QMainWindow window;
     window.setWindowTitle("Pac-Man Map");
     window.setCentralWidget(&mapView);
-
+    MainWindow w;
     window.show();
-
+    w.show();
     return a.exec();
 }

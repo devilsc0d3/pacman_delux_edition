8610a30669801424c6b7d68b0f5ea817442e0479
MedericFedora <kyomamh@gmail.com>
2025-06-24 13:52:30 +0200

    New

--- main.cpp
+++ main.cpp
@@ -8,28 +8,13 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    // 1. Créer l'objet Map (le modèle de données)
     Map gameMap;
-
-    // 2. Créer l'objet MapView (la vue graphique) en lui passant le modèle
     MapView mapView(gameMap);
-
-    // Facultatif : Définir une taille de cellule différente si vous voulez
-    // mapView.setCellSize(25);
-
-    // Facultatif : Utiliser la Map avec la PacGumFactory (comme prévu)
-    // PacGumFactory factory;
-    // QVector<GumEntity> gums = factory.populateMap(gameMap, BonusGenerationStrategy::BonusOnCorner);
-    // Vous auriez besoin de stocker et gérer ces gums, et potentiellement les dessiner aussi.
-
-    // 3. Créer une fenêtre principale pour contenir la vue
     QMainWindow window;
     window.setWindowTitle("Pac-Man Map");
-    window.setCentralWidget(&mapView); // Placez la MapView dans la fenêtre centrale
+    window.setCentralWidget(&mapView);
 
-    // 4. Afficher la fenêtre
     window.show();
 
-    // 5. Lancer la boucle d'événements de l'application Qt
     return a.exec();
 }

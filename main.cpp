b0fec4c716b5a672f9bc8987421ca465e8ab349c
Aubin <heuillet.aubin@gmail.com>
2025-06-30 14:04:02 +0200

    Linux works

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QApplication app(argc, argv);
+
 
     QMediaPlayer *player = new QMediaPlayer;
     QAudioOutput *audioOutput = new QAudioOutput;

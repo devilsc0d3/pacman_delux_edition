0df4fe9f1520abe318dd712d6c3b87734ad30570
devilsc0d3 <faureleo736@gmail.com>
2025-06-30 10:08:40 +0200

    feat: add resource file music

---
+++ settingswindow.h
@@ -0,0 +1,13 @@
+#ifndef SETTINGSWINDOW_H
+#define SETTINGSWINDOW_H
+
+#include <QWidget>
+
+class SettingsWindow : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit SettingsWindow(QWidget *parent = nullptr);
+};
+
+#endif // SETTINGSWINDOW_H 

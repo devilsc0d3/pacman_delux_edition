f4ea7a47c39a9c025b29ad64562fa74a73dfc257
Sasha <sasha.brun@ynov.com>
2025-06-30 10:02:10 +0200

    rules changed

--- rulesdialog.cpp
+++ rulesdialog.cpp
@@ -61,26 +61,28 @@ void RulesDialog::setupUI()
 
     QString rulesText =
         "<h1>But du jeu</h1>"
-        "<p>Le but du jeu est de manger toutes les Pac-Gommes dans le labyrinthe sans se faire attraper par les fantômes.</p>"
+        "<p>Ce mode de jeu se joue en deux équipes : l'équipe des Pac-Man et l'équipe des Fantômes.</p>"
 
-        "<h2>Déplacements</h2>"
-        "<p>Utilisez les touches fléchées (Haut, Bas, Gauche, Droite) pour déplacer Pac-Man dans le labyrinthe.</p>"
+        "<h2>Équipe Pac-Man</h2>"
+        "<p><b>Objectif :</b> Manger toutes les Pac-Gommes du labyrinthe pour marquer un maximum de points et gagner la partie.</p>"
+        "<p><b>Déplacements :</b> Utilisez les touches fléchées pour vous déplacer.</p>"
+        "<p><b>Pac-Gommes :</b> Les petites gommes rapportent des points. Les grosses Super Pac-Gommes vous rendent temporairement invincibles et vous permettent de manger les fantômes pour des points bonus.</p>"
+        "<p><b>Vies :</b> Chaque Pac-Man a un nombre de vies limité. Perdre toutes vos vies vous élimine de la partie.</p>"
 
-        "<h2>Les Pac-Gommes</h2>"
-        "<p>Mangez toutes les petites gommes pour passer au niveau suivant. Les grosses gommes, appelées Super Pac-Gommes, vous rendent temporairement invincible et vous permettent de manger les fantômes.</p>"
+        "<h2>Équipe Fantômes</h2>"
+        "<p><b>Objectif :</b> Attraper les Pac-Man pour les empêcher de vider le labyrinthe et de marquer des points. La victoire est à vous si vous empêchez les Pac-Man de finir le niveau.</p>"
+        "<p><b>Déplacements :</b> Utilisez les touches fléchées pour vous déplacer.</p>"
+        "<p><b>Comportement :</b> Si un Pac-Man mange une Super Pac-Gomme, vous devenez vulnérable. Si vous êtes mangé, vous réapparaissez à votre base après un court instant.</p>"
 
-        "<h2>Les Fantômes</h2>"
-        "<p>Évitez les fantômes ! S'ils vous touchent, vous perdez une vie. Après avoir mangé une Super Pac-Gomme, les fantômes deviennent bleus et vous pouvez les manger pour gagner des points supplémentaires. Ils réapparaîtront ensuite à leur base.</p>"
-
-        "<h2>Le Score</h2>"
+        "<h2>Score</h2>"
         "<ul>"
         "<li>Pac-Gomme : 10 points</li>"
         "<li>Super Pac-Gomme : 50 points</li>"
-        "<li>Manger un fantôme : 200, 400, 800, 1600 points (le score double pour chaque fantôme mangé successivement)</li>"
+        "<li>Manger un fantôme : 200, 400, 800, 1600 points</li>"
         "</ul>"
 
-        "<h2>Vies</h2>"
-        "<p>Vous commencez avec 3 vies. Vous perdez une vie chaque fois que vous êtes attrapé par un fantôme. Le jeu se termine lorsque vous n'avez plus de vies.</p>";
+        "<h2>Fin de la Partie</h2>"
+        "<p>La partie se termine si l'équipe Pac-Man mange toutes les Pac-Gommes, ou si tous les Pac-Man ont perdu toutes leurs vies.</p>";
 
     rulesLabel->setText(rulesText);
     scrollArea->setWidget(rulesLabel);
